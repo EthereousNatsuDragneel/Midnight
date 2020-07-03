7d17d8cabb61327db0c78bf27110f81fb3eb810b
ethereousnatsudragneel <mnjt.singh2000@gmail.com>
2020-07-03 21:02:03 +0530

    For Anirudh

--- src/utils/firebaseHandle.js
+++
@@ -1,8 +0,0 @@
-import firebase from "firebase";
-import 'firebase/firestore'
-import firebaseConfig from "../../config";
-class firebaseHandle{
-init(){firebase.initializeApp(firebaseConfig);}
-getDBInstance(path){return firebase.firestore().collection(path)}
-getInstance(){return firebase;}}
-export default new firebaseHandle();
